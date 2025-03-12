6a935369ef50255adbc79efda10dd1740534ffba
irhamkamil <irhamkamil8322@gmail.com>
2025-03-12 13:57:48 +0700

    Add: hover MovieCard

--- src/components/elements/MovieCard/index.jsx
+++ src/components/elements/MovieCard/index.jsx
@@ -1,11 +1,15 @@
+import { IoMdArrowDropright } from "react-icons/io";
+import { FaCheck } from "react-icons/fa6";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+
 import NewEpisode from "../NewEpisode";
 import TopRank from "../TopRank";
 const MovieCard = ({ image, title, rating, isNew, topRank, minWidth }) => {
   return (
     <div
-      className={`${minWidth} rounded-lg overflow-hidden relative brightness-90`}
+      className={`${minWidth} relative group rounded-lg transition-all duration-300 shadow-md hover:shadow-lg overflow-visible`}
     >
-      <img src={image} alt={title} className="w-full h-auto object-cover" />
+      <img src={image} alt={title} className="w-full h-full object-cover" />
       {isNew && <NewEpisode />}
       {topRank && <TopRank />}
       <div className="absolute bottom-2 left-2 bg-transparent bg-opacity-50 px-2 w-full text-white font-semibold">
@@ -13,6 +17,50 @@ const MovieCard = ({ image, title, rating, isNew, topRank, minWidth }) => {
       </div>
       <div className="absolute bottom-2 right-2 text-white bg-transparent bg-opacity-50 px-2 py-1 rounded-md text-sm">
         {rating ? `⭐ ${rating}` : ""}
+      </div>
+
+      <div
+        className="absolute bottom-0 left-0 w-full h-1/2 bg-black bg-opacity-80 opacity-0 
+        group-hover:opacity-100 transition-all duration-300 
+        z-10 "
+      >
+        {/* <img
+          src={image}
+          alt={title}
+          className="w-full h-1/2 object-cover rounded-t-lg"
+        /> */}
+        <div className=" flex flex-col justify-center gap-3 px-4 text-white">
+          <div className="flex justify-between mt-4">
+            <div className="flex gap-2">
+              <button
+                onClick={() => scroll("left")}
+                className="bg-white text-black w-7 h-7 rounded-full flex items-center justify-center shadow-md hover:shadow-lg transition-all duration-300 hover:bg-gray-300 cursor-pointer"
+              >
+                <IoMdArrowDropright size={30} />
+              </button>
+
+              <button
+                onClick={() => scroll("left")}
+                className="bg-transparent text-white w-7 h-7 outline-1 outline-white hover:outline-gray-300 rounded-full flex items-center justify-center shadow-md hover:shadow-lg transition-all duration-300 cursor-pointer"
+              >
+                <FaCheck size={15} />
+              </button>
+            </div>
+            <button
+              onClick={() => scroll("left")}
+              className="bg-transparent text-white w-7 h-7 outline-1 outline-white hover:outline-gray-300 rounded-full flex items-center justify-center shadow-md hover:shadow-lg transition-all duration-300 cursor-pointer"
+            >
+              <MdOutlineKeyboardArrowDown size={22} />
+            </button>
+          </div>
+          <div className="flex justify-center items-center gap-1 md:gap-5 text-gray-300 text-[0.5rem] md:text-sm">
+            <span>Misteri</span>
+            <span className="w-1 h-1 md:w-2 md:h-2 bg-gray-400 rounded-full"></span>
+            <span>Kriminal</span>
+            <span className="w-1 h-1 md:w-2 md:h-2 bg-gray-400 rounded-full"></span>
+            <span>Fantasi</span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/fragments/MovieSlider/index.jsx
+++ src/components/fragments/MovieSlider/index.jsx
@@ -22,15 +22,16 @@ export default function MovieSlider({
   };
 
   return (
-    <div className="w-full h-full mx-auto px-4 md:px-8 py-8 mt-4">
+    <div className="w-full h-full mx-auto px-4 md:px-8 py-8 mt-4 overflow-visible">
       <h2 className="text-white text-xl md:text-2xl font-bold mb-6">
         {mainTitle}
       </h2>
-      <div className="w-full relative flex items-center">
+
+      <div className="w-full relative flex items-center overflow-visible">
         {/* Left Arrow */}
         <button
           onClick={() => scroll("left")}
-          className="absolute left-1 md:-left-5 z-10 bg-button-2 text-white w-10 h-10 rounded-full flex items-center justify-center shadow-md hover:bg-gray-700"
+          className="absolute left-1 md:-left-5 z-20 bg-button-2 text-white w-10 h-10 rounded-full flex items-center justify-center shadow-md hover:bg-gray-700"
         >
           <IoArrowBackOutline size={20} />
         </button>
@@ -38,7 +39,7 @@ export default function MovieSlider({
         {/* Movie Slider */}
         <div
           ref={sliderRef}
-          className="w-full h-auto flex gap-4 overflow-x-scroll hide-scrollbar scroll-smooth"
+          className="w-full h-auto flex gap-4 overflow-x-auto hide-scrollbar scroll-smooth relative"
         >
           {movies.map((movie, index) => (
             <MovieCard
@@ -57,7 +58,7 @@ export default function MovieSlider({
         {/* Right Arrow */}
         <button
           onClick={() => scroll("right")}
-          className="absolute right-1 md:-right-5 z-10 bg-gray-800 text-white w-10 h-10 rounded-full flex items-center justify-center shadow-md hover:bg-gray-700"
+          className="absolute right-1 md:-right-5 z-20 bg-gray-800 text-white w-10 h-10 rounded-full flex items-center justify-center shadow-md hover:bg-gray-700"
         >
           <IoArrowForwardOutline size={20} />
         </button>
